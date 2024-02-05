d909f7ca1a5b4ee202928f57a1d977a4328ac906
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-04 22:00:22 -0300

    se corrigio ubicacion componente Titulo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Formulario from "./components/Formulario";
 import Footer from "./components/Footer";
-import Titulo from "./Titulo";
+import Titulo from "./components/Titulo";
 import { useState } from "react";
 import ListaNoticias from "./components/ListaNoticias";
 
 function App() {
-
-  const [categoria, setCategoria] = useState('world');
+  const [categoria, setCategoria] = useState("world");
 
   const handleCategoryChange = (newCategoria) => {
     setCategoria(newCategoria);
--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,42 +1,41 @@
 import { Row, Col } from "react-bootstrap";
 import Noticia from "./Noticia";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ListaNoticias = ({ categoria }) => {
-    const [noticias, setNoticias] = useState([]);
-    const apiKey = 'pub_374867eb717c36cfe3081169e0a55ea02fe14'
+  const [noticias, setNoticias] = useState([]);
+  const apiKey = "pub_374867eb717c36cfe3081169e0a55ea02fe14";
 
   useEffect(() => {
     fetchNoticias(categoria);
   }, [categoria]);
 
   const fetchNoticias = (categoria) => {
-    fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&country=au,us&category=${categoria}`)
-      .then(resp => {
+    fetch(
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&country=au,us&category=${categoria}`
+    )
+      .then((resp) => {
         if (!resp.ok) {
-          throw new Error('Respuesta de red incorrecta');
+          throw new Error("Respuesta de red incorrecta");
         }
         return resp.json();
       })
-      .then(data => {
+      .then((data) => {
         setNoticias(data.results || []);
       })
-      .catch(error => {
-        console.error('Error al recuperar noticias:', error);
+      .catch((error) => {
+        console.error("Error al recuperar noticias:", error);
       });
   };
-  
 
   return (
     <div className="py-3 mt-3">
       <Row className="d-flex justify-content-center">
-      {noticias.map((item, index) => (
-        <Col md={4} className="mb-4" key={index}>
-        <Noticia  item={item} />
-        </Col>
-      ))}
-        
-        
+        {noticias.map((item, index) => (
+          <Col md={4} className="mb-4" key={index}>
+            <Noticia item={item} />
+          </Col>
+        ))}
       </Row>
     </div>
   );
